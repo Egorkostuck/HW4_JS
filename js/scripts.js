52061c22bf96c5b92649d235a27299dcfcb518bc
Egor <turak_09@mail.ru>
2020-12-14 23:24:02 +0300

    new

--- js/scripts.js
+++ js/scripts.js
@@ -57,9 +57,10 @@ document.write('</br>' + 'task 4' + '</br>');
 
 let arr5 = ['10', '20', '30', '50', '235', '3000'];
 let h = 0;
-while(h <= arr5.length) {
-    if (h[0] !== '3', typeof arr5[h] !== 'undefined')
-    document.write(arr5[h]);
+while(h < arr5.length) {
+    if (arr5[h][0] !== '3') {
+        document.write(arr5[h]);
+    }
     h++;
 }
 
@@ -69,10 +70,14 @@ document.write('</br>' + 'task 5' + '</br>');
 
 let arr6 = ['ПН','ВТ','СР','ЧТ','ПТ','СБ','ВС'];
 let g=0;
-while (g <= arr6.length, typeof arr6[g] == 'undefined') {  
-    if (g[0] !== 'C' && g[1] !== 'Б' || g[0] !== 'В' && g[1] !== 'C')    
+while (g < arr6.length) {  
+    if (arr6[g] == 'СБ' || arr6[g] == 'ВС')  {
+        arr6[g] = '<b>' + arr6[g] + '</b>';
+    }  
+    document.write (arr6[g]); 
     g++; 
-} document.write ('<b>'+arr6[g]+'</b>');
+    
+}
 
 
 //task 6
@@ -91,19 +96,29 @@ document.write(arr7[arr7.length-1]);
 //task 7
 document.write('</br>' + 'task 7' + '</br>');
 
-let a = +prompt('Введите число, чтобы выйти из ввода введите пустое значение:');
+let a = prompt('Введите число, чтобы выйти из ввода введите пустое значение:');
 let arr10 = [];
-while ( Number.isNaN(NaN), a != '' ) {
-    a = +prompt('Введите число, чтобы выйти из ввода введите пустое значение:');
-    arr10.push(a);
-}alert('Ошибка ввода');
+while ( a !== '' && a !== undefined && a != null) {
+          
+    if (a != '' || a !== undefined || a != null && !isNaN(a)){
+      arr10.push(a);
+    }
+    else{
+        
+      alert('ошибка ввода');
+    }
 
+    a = prompt('Введите число, чтобы выйти из ввода введите пустое значение:');
+    
+}
 document.write(arr10);
 arr10.sort(function(a,b) {
     return a-b;
 });
 document.write('</br>');
-document.write('Сортировка:' + arr10);
+document.write('Сортировка:' + arr10); 
+
+
 
 
 //task 8
@@ -133,10 +148,43 @@ let arr11 = [5, 9, 21,,,9,78,,,,6];
 let summ = 0;
 let p = arr11.length;
 while (p--) {
-    if (typeof arr11[p] === 'undefined') 
+    if (arr11[p] == undefined) 
     summ++;
 }
 document.write(summ);
 
 //task 10
 document.write('</br>' + 'task 10' + '</br>');
+
+let arr12 = [48,9,0,4,21,2,1,0,8,84,76,8,4,13,2];
+let summ2 = 0;
+for (let i = arr12.indexOf(0); i < arr12.lastIndexOf(0); i++){
+    summ2 += arr12[i];
+}
+document.write(summ2);
+
+//task 11
+document.write('</br>' + 'task 11' + '</br>');
+
+let l = +prompt('ВВедите высоту:'),
+    length = l*2 - 1,
+    middle = (length - 1)/2;
+
+for (let i=0; i<l; i++){
+    let start = middle - i,
+        end = middle + i;
+    
+    for (let k=0; k<length; k++){
+        if ( k >= start && k <= end) {
+            document.write('*');
+        }
+        else {
+            document.write('$');
+        }
+    }
+    document.write('</br>');
+}
+
+
+
+console.log(!!"");
